a7abe12935b4045cc44bd32bc8d70a786503d091
Isaac Angulo <isaac.angulogomez@gmail.com>
2026-06-11 10:50:10 -0700

    menu js typo

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,5 +2,5 @@ const menu = document.querySelector('.menu');
 
 menu?.addEventListener('click', () => {
     const isExpanded = menu.getAttribute('aria-expanded') === 'true';
-    menu.selecAttribute('aria-expanded', `${!isExpanded}`);
+    menu.setAttribute('aria-expanded', `${!isExpanded}`);
 });
